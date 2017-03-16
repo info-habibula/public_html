3f4421f1ea1390cde86e5db05384a5da9e053532
site-razhihan <site.info94@gmail.com>
2017-03-16 16:44:59 +0300

    Регистрация

---
+++ form_reg.php
@@ -0,0 +1,101 @@
+<?php
+error_reporting(E_ALL);
+#ini_set('display_errors', 1);
+ini_set('error_append_string', 'Биля :(');
+ini_set('log_errors', 'On');
+ini_set('error_log', '/var/log/php_errors.log');
+$user="root";
+$pass="1";
+?>                                                                                                            
+<html>
+ 
+ <head>
+   <meta charset="utf-8">
+   <title> Kалькулятор +</title>
+   <style type="text/css">
+  body {
+    background: #F5F5F5;
+    background-image: -webkit-gradient(
+        linear,
+        left top,
+        right top,
+        color-stop(.34, rgba(230,237,241,.05)),
+        color-stop(.67, rgba(230,237,241,0)));
+    -webkit-background-size: 5px 5px;
+} </style>
+ </head>
+    <body>
+        <center><p><b>Выполните вход что бы получить VIP-версию нашего калькулятора ©</2b></p>
+        <form action="" id="loginform" method="post" name="loginform">
+            <p><label for="user_login">Имя пользователя<br>
+            <input class="input" id="username" name="username"size="20"
+            type="text" value="" required></label></p>
+            <p><label for="user_pass" >Пароль<br>
+            <input class="input" id="password" name="password"size="20"
+            type="password" value="" required></label></p> 
+            <button type="submit">Войти</button>
+            <p class="regtext">Еще не зарегистрированы?<a href= "register.php">Регистрация</a>!</p>
+        </form></center>
+
+
+<!--<form>
+<p><b> введите первое число...</b> <input type="text" autocomplete="off" name="chislo1" required> <Br> </p>
+<p><b> введите второе число... </b> <input type="text" autocomplete="off" name="chislo2" required> <Br> </p> 
+<p><b>Выберите действие</b>
+<select name="znaki"> 
+<option value="-">минус</option>
+<option value="*">умножить</option>
+</select></p>
+<button type="submit">Вычислить</button>-->
+    <?php
+        $chislo1=$_GET['chislo1'];
+        $chislo2=$_GET['chislo2'];
+        $znaki=$_GET['znaki'];
+        $rezultat=$_GET['rezultat'];
+           switch ($znaki) {
+            case '-':
+                $rezultat=$chislo1 - $chislo2;
+                echo "это результат $rezultat <br>";
+                echo 'это результат $rezultat';
+                $oper=$chislo1 ."-". $chislo2. '='. $rezultat;
+            break;
+            case '*':
+                $rezultat=$chislo1 * $chislo2;
+                $oper=$chislo1 ."*". $chislo2. '='. $rezultat;
+                    break;
+        } 
+        try {
+            $dbh = new PDO('mysql:host=localhost;dbname=patya', $user, $pass);
+            $sql = 'insert into colculator (operation, data_vip) value ("'.$oper.'", "'.date("Y-m-d H:i:s").'")';
+
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute();
+            $sql= 'select * from colculator where opertion_id>100;';
+            $history= $dbh->query($sql);
+            /*foreach ($history as $key => $value) {
+                    echo "<pre>";
+                    var_dump ($value);
+                    echo "</pre>";
+                
+                foreach ($value as $key2 => $value2) {
+                    echo "<pre>";
+                    var_dump ($value2);
+                    echo "</pre>";
+                }
+                
+            }*/
+            die();
+            $dbh = null;
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    ?>
+ <input type="text" autocomplete="off" name="" value="<?php echo $oper; ?>" /></Br></br>
+ </form>
+ </body>
+ </html>
+
+
+
+
--- index.php
+++ index.php
@@ -4,74 +4,43 @@ ini_set('display_errors', 1);
 ini_set('error_append_string', 'Биля :(');
 ini_set('log_errors', 'On');
 ini_set('error_log', '/var/log/php_errors.log');
-$user="root";
-$pass="1";
 ?>                                                                                                            
 <html>
  
  <head>
    <meta charset="utf-8">
-   <title> Kалькулятор +</title>
+   <title> Колькулейшен</title>
+    <style type="text/css">
+  body {
+    background: #F5F5F5;
+    background-image: -webkit-gradient(
+        linear,
+        left top,
+        right top,
+        color-stop(.34, rgba(230,237,241,.05)),
+        color-stop(.67, rgba(230,237,241,0)));
+    -webkit-background-size: 5px 5px;
+ }
+  
+  input {
+   background: -moz-linear-gradient(#D0ECF4, #5BC9E1, #D0ECF4);
+   background: -webkit-gradient(linear, 0 0, 0  100%, from(#D0ECF4), to(#D0ECF4), color-stop(0.5, #5BC9E1));
+   filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#00BBD6', endColorstr='#EBFFFF');
+   padding: 3px 7px;
+   color: #333;
+   -moz-border-radius: 5px;
+   -webkit-border-radius: 5px;
+   border-radius: 5px;
+   border: 1px solid #666;
+   margin-top: 20%;
+   font-size: 25pt;
+
+  }
+  </style>
  </head>
     <body>
-<form>
-<p><b> введите первое число...</b> <input type="text" autocomplete="off" name="chislo1" required> <Br> </p>
-<p><b> введите второе число... </b> <input type="text" autocomplete="off" name="chislo2" required> <Br> </p> 
-<p><b>Выберите действие</b>
-<select name="znaki"> 
-<option value="-">минус</option>
-<option value="*">умножить</option>
-</select></p>
-<button type="submit">Вычислить</button>
-<?php
-$chislo1=$_GET['chislo1'];
-$chislo2=$_GET['chislo2'];
-$znaki=$_GET['znaki'];
-$rezultat=$_GET['rezultat'];
-   switch ($znaki) {
-    case '-':
-        $rezultat=$chislo1 - $chislo2;
-        echo "это результат $rezultat <br>";
-        echo 'это результат $rezultat';
-        $oper=$chislo1 ."-". $chislo2. '='. $rezultat;
-    break;
-    case '*':
-        $rezultat=$chislo1 * $chislo2;
-        $oper=$chislo1 ."*". $chislo2. '='. $rezultat;
-            break;
-} 
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=patya', $user, $pass);
-
-    $sql = 'insert into colculator (operation, data_vip) value ("'.$oper.'", "'.date("Y-m-d H:i:s").'")';
-
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
-    $sql= 'select * from colculator where opertion_id>100;';
-    $history= $dbh->query($sql);
-    foreach ($history as $key => $value) {
-            echo "<pre>";
-            var_dump ($value);
-            echo "</pre>";
-        
-        foreach ($value as $key2 => $value2) {
-            echo "<pre>";
-            var_dump ($value2);
-            echo "</pre>";
-        }
-        
-    }
-    die();
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
- ?>
- <input type="text" autocomplete="off" name="" value="<?php echo $oper; ?>" /></Br></br>
- </form>
- </body>
+      <center>  <form action="/form_reg.php" method="POST" >
+            <input name="myActionName"  type="submit" value="Колькулятор" style="width:300Px;height:80Px"/>
+        </form> </center>
+    </body>
  </html>
-
-
-
