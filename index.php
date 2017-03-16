cc3f389d3177452c446a4e7709ab25f923a57ef0
habibula <info.habibula@gmail.com>
2017-03-16 15:16:49 +0300

    калькулятор

--- index.php
+++ index.php
@@ -1,32 +1,50 @@
+<?php
+$user="root";
+$pass="1";
+?>                                                                                                            
 <html>
  <head>
    <meta charset="utf-8">
-   <title> Kалькулятор </title>
+   <title> Kалькулятор</title>
  </head>
     <body>
 <form>
-<p><b> введите первое число...</b> <input type="text" name="chislo1"> <Br> </p>
-<p><b> введите второе число... </b> <input type="text" name="chislo2"> <Br> </p> 
+<p><b> введите первое число...</b> <input type="text" autocomplete="off" name="chislo1" required> <Br> </p>
+<p><b> введите второе число... </b> <input type="text" autocomplete="off" name="chislo2" required> <Br> </p> 
 <p><b>Выберите действие</b>
 <select name="znaki"> 
-<option value="-">минус</option>
-<option value="*">умножить</option>
+<option value="+">+</option>
+<option value="/">/</option>
 </select></p>
- 		<p><button type="submit">Вычислить</button> <input  type="text" name="rezultat"></p>
-</form>
- </body>
- <?php
+<button type="submit">Вычислить</button>
+<?php
 $chislo1=$_GET['chislo1'];
 $chislo2=$_GET['chislo2'];
 $znaki=$_GET['znaki'];
 $rezultat=$_GET['rezultat'];
    switch ($znaki) {
-   	case '-':
-       echo $chislo1 - $chislo2;
-   	break;
-   	case '*':
-       echo $chislo1 * $chislo2;
-   	break;
+    case '+':
+        $rezultat=$chislo1 + $chislo2;
+        $oper=$chislo1 ."+". $chislo2. '='. $rezultat;
+    break;
+    case '/':
+        $rezultat=$chislo1 / $chislo2;
+        $oper=$chislo1 ."/". $chislo2. '='. $rezultat;
+            break;
 } 
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=patya', $user, $pass);
+    $sql = 'insert into colculator (operation_vip, data_vip) value ("'.$oper.'", "'.date("Y-m-d H:i:s").'")';
+    
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
  ?>
+ <input type="text" autocomplete="off" name="" value="<?php echo $oper; ?>" /></Br></br>
+ </form>
+ </body>
  </html>
