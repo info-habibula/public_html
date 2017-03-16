3911920b8ed470dc2d60c2cc1aa824fcd8c663c5
site-razhihan <site.info94@gmail.com>
2017-03-16 12:59:42 +0300

    Тестовый

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('error_append_string', 'Биля :(');
+ini_set('log_errors', 'On');
+ini_set('error_log', '/var/log/php_errors.log');
 $user="root";
 $pass="1";
 ?>                                                                                                            
@@ -25,6 +30,8 @@ $rezultat=$_GET['rezultat'];
    switch ($znaki) {
     case '-':
         $rezultat=$chislo1 - $chislo2;
+        echo "это результат $rezultat <br>";
+        echo 'это результат $rezultat';
         $oper=$chislo1 ."-". $chislo2. '='. $rezultat;
     break;
     case '*':
@@ -36,9 +43,24 @@ try {
     $dbh = new PDO('mysql:host=localhost;dbname=patya', $user, $pass);
 
     $sql = 'insert into colculator (operation, data_vip) value ("'.$oper.'", "'.date("Y-m-d H:i:s").'")';
-    
+
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
+    $sql= 'select * from colculator where opertion_id>100;';
+    $history= $dbh->query($sql);
+    foreach ($history as $key => $value) {
+            echo "<pre>";
+            var_dump ($value);
+            echo "</pre>";
+        
+        foreach ($value as $key2 => $value2) {
+            echo "<pre>";
+            var_dump ($value2);
+            echo "</pre>";
+        }
+        
+    }
+    die();
     $dbh = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
